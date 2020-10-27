52462886271d49b8e0803987cd141398d041bb60
Subrat Behera <sb54086@gmail.com>
2020-10-27 11:11:36 +0530

    Update MongoGradleApplication.java

--- mongo-gradle/src/main/java/com/example/app/MongoGradleApplication.java
+++ mongo-gradle/src/main/java/com/example/app/MongoGradleApplication.java
@@ -1,13 +1,19 @@
 package com.example.app;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongoGradleApplication {
+	private static final Logger LOGGER= LoggerFactory.getLogger(MongoGradleApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoGradleApplication.class, args);
+		LOGGER.info("Application started {}");
 	}
 
 }
